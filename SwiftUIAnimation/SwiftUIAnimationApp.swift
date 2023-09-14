027da3eaf61521030bce5761cbe5f40c6538d5ef
Ankit Sharma <ankitsoftengi@gmail.com>
2023-09-14 10:51:57 +0530

    Initial Commit

---
+++ SwiftUIAnimation/SwiftUIAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAnimationApp.swift
+//  SwiftUIAnimation
+//
+//  Created by Ankit Sharma on 14/09/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
